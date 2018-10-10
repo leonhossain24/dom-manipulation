7c580fbf31012ff3a2dcb2dd838b0b95330fa422
leonhossain <leon.hossain2427@gmail.com>
2018-10-10 03:29:09 +0000

    master

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
 /* Task 1 */
+var heading = document.getElementById('heading')
+
+console.log(heading.innerText);
+
+heading.innerText = '<h3>Asa Cat!</h3>'
 
 
 /* Task 2 using plain JavaScript */
+//task 2a
+var textBtn = document.getElementById('addText2');
+textBtn.addEventListener('click', function(event){
+    var task2 = document.getElementById('task2a');
+   
+    var paragraph = document.createElement('p');
+     
+    paragraph.innerText = 'Hello World!';
+   
+    task2.appendChild(paragraph);
+});
+
+//task 2b
+function changeBGcolor(event)
+{
+    var body = document.getElementsByTagName('body')[0];
+    // if(event.target.innerText == 'Red')
+    // {
+    //     body.style.background = 'red';
+    // }
+    // if(event.target.innerText == 'Green')
+    // {
+    //     body.style.background = 'green';
+    // }
+    
+    body.style.backgroundColor = event.target.innerText.toLowerCase();
+    
+}
+
+
 
 
 
